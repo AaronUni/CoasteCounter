40c0a9beb05edc2bea6e3c683923e3292fdef4b0
AaronUni <Aaron.pohlert@stud.th-luebeck.de>
2021-01-06 19:52:09 +0100

    toolbar mit logo

--- app/src/main/java/com/example/coastecounter/MainActivity.java
+++ app/src/main/java/com/example/coastecounter/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.coastecounter;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
     }
 }
