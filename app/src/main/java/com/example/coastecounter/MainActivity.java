68cc8556e2e7a3388d3ae47b43cd527e5e998642
AaronUni <Aaron.pohlert@stud.th-luebeck.de>
2021-01-06 20:37:36 +0100

    toolbar bearbeitet

--- app/src/main/java/com/example/coastecounter/MainActivity.java
+++ app/src/main/java/com/example/coastecounter/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.coastecounter;
 
+import android.view.Menu;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
@@ -12,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.navigation, menu);
+        return true;
+    }
+
 }
