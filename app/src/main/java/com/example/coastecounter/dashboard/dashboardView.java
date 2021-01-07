c8989ab790bdd8e617fbe1c4fb5bdc8426de076d
Kai-Swenson <kai.swenson@stud.th-luebeck.de>
2021-01-07 16:24:16 +0100

    CoasterCount ist neue main darüber wird das wechseln der View geregelt MainActivity in dashboardView umbenannt

--- app/src/main/java/com/example/coastecounter/dashboard/dashboardView.java
+++ app/src/main/java/com/example/coastecounter/dashboard/dashboardView.java
@@ -1,4 +1,4 @@
-package com.example.coastecounter.main;
+package com.example.coastecounter.dashboard;
 
 import android.content.res.Resources;
 import android.view.Menu;
@@ -7,25 +7,26 @@ import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
 import com.example.coastecounter.R;
 
-public class MainActivity extends AppCompatActivity {
+public class dashboardView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.dashboard);
+      super.onCreate(savedInstanceState);
+  /*        setContentView(R.layout.dashboard);
         Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
         setTitle(R.string.title_dashboard);
        /* getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);*/        //wird benötigt für den Backpfeil  https://stackoverflow.com/questions/35810229/how-to-display-and-set-click-event-on-back-arrow-on-toolbar
 
-
+/**/
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.navigation, menu);
+   /*     // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.navigation, menu); */
         return true;
+
     }
 
 }
--- app/src/main/java/com/example/coastecounter/main/CoasterCount.java
+++ app/src/main/java/com/example/coastecounter/main/CoasterCount.java
@@ -1,5 +1,10 @@
 package com.example.coastecounter.main;
 
+import android.os.Bundle;
+import android.view.Menu;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import com.example.coastecounter.R;
 import com.example.coastecounter.achterbahn.AchterbahnController;
 import com.example.coastecounter.badge.BadgeController;
 import com.example.coastecounter.badge.BadgeKategorieController;
@@ -8,24 +13,44 @@ import com.example.coastecounter.bewertung.BewertungsController;
 import com.example.coastecounter.park.ParkController;
 import com.example.coastecounter.park.ParkRepo;
 
-public class CoasterCount {
+public class CoasterCount extends AppCompatActivity {
 
-	private BewertungsController bewertungsController;
+    private BewertungsController bewertungsController;
 
-	private ParkController parkController;
+    private ParkController parkController;
 
-	private BenutzerController benutzerController;
+    private BenutzerController benutzerController;
 
-	private BadgeKategorieController badgeKategorieController;
+    private BadgeKategorieController badgeKategorieController;
 
-	private AchterbahnController achterbahnController;
+    private AchterbahnController achterbahnController;
 
-	private BadgeController badgeController;
+    private BadgeController badgeController;
 
-	private ParkRepo parkRepo;
+    private ParkRepo parkRepo;
 
-	public void main() {
+    public void main() {
 
-	}
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+       /* getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);*/        //wird benötigt für den Backpfeil  https://stackoverflow.com/questions/35810229/how-to-display-and-set-click-event-on-back-arrow-on-toolbar
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.navigation, menu);
+        return true;
+    }
+
 
 }
