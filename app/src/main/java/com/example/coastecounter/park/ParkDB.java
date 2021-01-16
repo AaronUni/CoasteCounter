403ba44ff3637a1f44ef487e8bac271155a694ce
Lukas-Quader <lukas.quader@stud.th-luebeck.de>
2021-01-16 02:16:36 +0100

    weiter Park hinzugefügt

--- app/src/main/java/com/example/coastecounter/park/ParkDB.java
+++ app/src/main/java/com/example/coastecounter/park/ParkDB.java
@@ -3,5 +3,9 @@ package com.example.coastecounter.park;
 public class ParkDB {
     Park heidePark = new Park(1, "Heide Park Resort", "info@heidepark.de", "Heide Park 1,\n29614 Soltau",
             "01805-919101","01805-545801", null, 1480000, null, null, 85,
-            "Das Heide Park Resort ist auf der Fläche deutschlands zweit größter Freizeitpark...,", "www.heide-park.de", null, null, null, null,null);
+            "Das Heide Park Resort ist auf der Fläche deutschlands zweit größter Freizeitpark...,", "www.heide-park.de",
+            null, null, null, null,null);
+    Park hansapark = new Park(2, "Hansa Park", "info@hansapark.de", "Am Fahrenkrog 1, \n23730 Sierksdorf", "04563-4740", "04563-474100",
+            null, 1400000, null, null, 46, "Deutschlands einziger Erlebnispark am Meer liegt in der Lübecker Bucht",
+            "www.hansapark.de", null, null, null, null, null);
 }
